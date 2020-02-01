5f25a1cce058385e1229ac6f37243a3b92aa2e40
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-02-01 18:13:48 -0400

    Terminado

--- Poligonos/Program.cs
+++ Poligonos/Program.cs
@@ -4,9 +4,63 @@ namespace Poligonos
 {
     class Program
     {
+        public class Poligono
+        {
+            private int Lados;
+            private int Distancia;
+
+            public int lados
+            {
+                get
+                {
+                    return Lados;
+                }
+                set
+                {
+                    if (value >= 3)
+                        Lados = value;
+                    else
+                        Lados = 0;
+                }
+            }
+
+            public int distancia
+            {
+                get
+                {
+                    return Distancia;
+                }
+                set
+                {
+                    if (value > 0)
+                        Distancia = value;
+                    else
+                        Distancia = 0;
+                }
+            }
+            public Poligono()
+            {
+                Lados = 0;
+                Distancia = 0;
+            }
+
+            public Poligono(int lados, int distancia)
+            {
+                Lados = lados;
+                Distancia = distancia;
+            }
+
+            public override string ToString()
+            {
+                String mensaje = "";
+                mensaje += "Los lados del poligono son " + Lados.ToString() +
+                            " y su distancia es " + Distancia.ToString();
+                return mensaje;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informacion sobre Poligonos");
         }
     }
 }
