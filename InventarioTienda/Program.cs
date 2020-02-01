2b4e53dbda97c0399aeda06c67b4f156d599dd70
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-02-01 17:54:13 -0400

    Add project files.

---
+++ InventarioTienda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InventarioTienda
+{
+    public class ClaseTienda
+    {
+        public float Precio { get; set; }
+        public int Unidades { get; set; }
+        public String Nombre { get; set; }
+
+        public ClaseTienda()
+        {
+            Precio = 0;
+            Unidades = 0;
+            Nombre = string.Empty;
+        }
+
+        public ClaseTienda(float precio, int unidades, String nombre)
+        {
+            Precio = precio;
+            Unidades = unidades;
+            Nombre = nombre;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventario de una Tienda\n");
+        }
+    }
+}
+
