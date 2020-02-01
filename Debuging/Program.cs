a587c674e111502b12dfad2ba8347002ec4f4ed9
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-02-01 18:20:22 -0400

    Add project files.

---
+++ Debuging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Debuging
+{
+    
+    class Program
+    {
+        public static int factorial(int Num)
+        {
+            int Resultado = 1;
+
+            for (int i = Num; i > 1; i--)
+            {
+                Resultado *= i;
+            }
+
+            return Resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ver Cambio de Variable\n");
+
+            int Numero;
+
+            Console.Write("Ingrese un numero: ");
+            Numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El factorial de {0} es {1}", Numero, factorial(Numero));
+        }
+    }
+    
+}
